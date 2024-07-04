8afe6f4a047daf7d8d945dd57855550cc1860374
Ginger Society Admin <admin@gingersociety.org>
2024-07-04 23:42:24 +0530

    feat: added db-compose render command at the startup

--- src/apps.py
+++ src/apps.py
@@ -2,8 +2,15 @@
 """App definition for the src app"""
 
 from ginger.apps import AppConfig
+import subprocess
 
 
 class SrcConfig(AppConfig):
     default_auto_field = "ginger.db.models.BigAutoField"
     name = "src"
+
+    def ready(self):
+        try:
+            subprocess.run(['db-compose', 'render', '--skip'], check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error occurred while running db-compose render: {e}")
