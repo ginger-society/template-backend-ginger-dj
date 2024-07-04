4147447740926afa96a4c1c764da61c9908e00b2
Ginger Society Admin <admin@gingersociety.org>
2024-07-04 13:56:50 +0530

    feat: added db support

--- src/admin.py
+++ src/admin.py
@@ -5,20 +5,3 @@ Admin module for the app src
 from ginger.contrib import admin
 
 from .models import *
-
-# admin.site.register(Test)
-admin.site.register(TestAppModel1)
-admin.site.register(Many2ManyTest)
-
-
-@admin.register(Test)
-class TestAdmin(admin.ModelAdmin):
-    list_display = ('char_field', 'choice_field', 'bool_field', 'positive_integer_field', 'field3')
-    list_filter = ('choice_field', 'bool_field', 'field3')
-    search_fields = ('char_field',)
-    ordering = ('char_field',)
-    fieldsets = (
-        (None, {
-            'fields': ('char_field', 'choice_field', 'bool_field', 'positive_integer_field', 'field3')
-        }),
-    )
--- src/models.py
+++ src/models.py
@@ -1,129 +1,58 @@
-"""Models"""
-
 from ginger.db import models
 
-# Create your models here.
 
+Course_Typeenum = (
+    ("compulsary" , "Compulsary"),
+    ("elective" , "Elective"),
 
-class Tenant2(models.Model):
-    """Tenant model , this is equivalent to an organization or BU unit"""
-
-    name = models.CharField(max_length=200, unique=True)
-    name2 = models.CharField(max_length=200, null=True)
-    is_active = models.BooleanField(default=True)
-    expiry_date = models.DateField(null=True, blank=True)
-    # this will be resolved once we add pylint_django
-    # pylint: disable=E0307
-
-    def __str__(self):
-        return self.name
-
-
-CHOICES = (
-    ("choice1", "choice1"),
-    ("choice2", "choice2"),
-    ("choice3", "choice3"),
 )
 
 
-class Test(models.Model):
-    """test"""
+Subjectenum = (
+    ("english" , "English"),
+    ("hindi" , "Hindi"),
+    ("maths" , "Maths"),
+    ("science" , "Science"),
+    ("social_studies" , "Social Studies"),
 
-    choice_field = models.CharField(null=True, choices=CHOICES, max_length=50, default="choice1")
-    bool_field = models.BooleanField(default=True)
-    char_field = models.CharField(null=True, max_length=50)
-    positive_integer_field = models.PositiveIntegerField(null=False, default=0)
-    field3 = models.BooleanField(default=False)
-
-
-class TestAppModel1(models.Model):
-    """test app model"""
-
-    char_field = models.CharField(max_length=200, null=False)
-
-
-class Many2ManyTest(models.Model):
-    """m2m test"""
-
-    name = models.CharField(max_length=200, null=False)
-    testModels = models.ManyToManyField(Test, related_name="main_models")
-    crossAppModel = models.ManyToManyField(TestAppModel1, related_name="cross_app_m2mtest")
-
-
-course_type = (
-    ("compulsary", "Compulsary"),
-    ("elective", "Elective"),
 )
 
 
-class student(models.Model):
-
-    name = models.CharField(
-        max_length=150,
-    )
-
-    roll_number = models.CharField(
-        max_length=40,
-    )
-
-    on_scholarship = models.BooleanField(
-        default=True,
-    )
-
-    father_name = models.CharField(
-        max_length=100,
-        null=True,
-    )
-
-    address = models.TextField(
-        max_length=500,
-    )
-
-    data_of_birth = models.DateField(
-        null=True,
-    )
-
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-    )
-
-    updated_at = models.DateField(
-        auto_now=True,
-    )
+class Student(models.Model):
+    name = models.CharField(max_length=150, null=False)
+    roll_number = models.CharField(max_length=40, null=False)
+    on_scholarship = models.BooleanField(default=True, null=False)
+    father_name = models.CharField(max_length=100, null=True)
+    address = models.TextField(max_length=500, null=False)
+    data_of_birth = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=False)
+    updated_at = models.DateField(auto_now=True, null=False)
+    has_cab_service = models.BooleanField(null=True)
 
     class Meta:
-        db_table = "student"
+        managed = False
 
 
-class enrollment(models.Model):
-
-    student = models.ForeignKey(
-        "student",
-        related_name="courses",
-        on_delete=models.DO_NOTHING,
-    )
-
-    course = models.ForeignKey(
-        "course",
-        on_delete=models.SET_NULL,
-        null=True,
-    )
+class Enrollment(models.Model):
+    student = models.ForeignKey("Student", on_delete=models.DO_NOTHING, null=False)
+    course = models.ForeignKey("Course", on_delete=models.SET_NULL, null=True)
 
     class Meta:
-        db_table = "enrollment"
+        managed = False
 
 
-class course(models.Model):
-
-    name = models.CharField(
-        max_length=100,
-    )
-
-    type = models.CharField(
-        choices=course_type,
-        max_length=50,
-        default="compulsary",
-    )
+class Course(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    course_type = models.CharField(default="compulsary", choices=Course_Typeenum, null=False)
+    duration = models.PositiveIntegerField(null=True)
 
     class Meta:
-        db_table = "course"
+        managed = False
+
+
+class Exam(models.Model):
+    date = models.DateField(auto_now_add=True, null=False)
+    subject = models.CharField(choices=Subjectenum, null=False)
+
+    class Meta:
+        managed = False
